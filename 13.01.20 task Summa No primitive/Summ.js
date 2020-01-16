d78326dada8476a94e2bb72c0b6bbefdda1c6b3c
Olexandr <58948441+flyborn@users.noreply.github.com>
2020-01-16 10:57:33 +0200

    version two
    make in function

---
+++ 13.01.20 task Summa No primitive/Summ.js
@@ -0,0 +1,38 @@
+// 13 января 2020 таск на сравнения
+
+var a = 35;
+var b = 2;
+var sum = a + b;
+console.log(sum);
+alert(sum);
+if(a >= 10){
+    onesum();
+}else{
+    nonsum();
+}
+function onesum(res, litl,big,trash){
+}if(sum >= 30){
+    var res = sum / 15;
+    console.log(res);
+    alert(res);
+}else if( sum < 10 ){
+    var litl = b - sum;
+    console.log(litl);
+    alert(litl);
+}if(sum > 10){
+    var big = a + sum;
+    console.log(big);
+    alert(big);
+}if(sum = a + b){
+    var trash = sum / 100;
+    console.log(trash);
+    alert(trash);
+}
+function nonsum(minus){
+    if(a < 10){
+    var minus = b - a;
+    console.log(minus);
+    alert(minus);
+}
+}
+
